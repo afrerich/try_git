3b87e9a2cc4d789e5e723d8e39da61a384024c72
Aaron Frerichs <afrerich@gmail.com>
2021-04-19 09:29:55 -0500

    update failing tests

--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Animal do
   let(:animal) {described_class.new}
 
   it "has Cow in the list of animals" do
-    expect(animal.list).to include("Cow")
+    expect(animal.list).to include("Cat")
   end
 end
 
--- spec/breakfast_spec.rb
+++ spec/breakfast_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Breakfast do
   let(:breakfast) {described_class.new}
 
   it "has Waffles in the list of breakfasts" do
-    expect(breakfast.list).to include("Waffles")
+    expect(breakfast.list).to include("Bacon")
   end
 end
 
