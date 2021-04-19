44e217c4f6e21c2d4d674a1ff9ae4f1ee4e4ba33
Aaron Frerichs <afrerich@gmail.com>
2021-04-19 09:41:38 -0500

    add failing test for waffles in breakfasts

--- spec/breakfast_spec.rb
+++ spec/breakfast_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe Breakfast do
   it "has Bascon in the list of breakfasts" do
     expect(breakfast.list).to include("Bacon")
   end
+
+  it "has Waffles in the list of breakfasts" do
+    expect(breakfast.list).to include("Waffles")
+  end
+
 end
 
